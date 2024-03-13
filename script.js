974e223003a23b9cbfa8a3e3ac18da04dba3258e
paulex1418 <olawaleronke5@gmail.com>
2024-03-13 11:47:37 +0100

    template literal

---
+++ script.js
@@ -0,0 +1,23 @@
+const firstName = "Jonas";
+const job = "teacher";
+const birthYear = 1991;
+const year = 2037;
+
+const jonas =
+  "I'm " + firstName + ", a " + (year - birthYear) + " year old " + job + "!";
+console.log(jonas);
+
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
+console.log(jonasNew);
+
+console.log(`Just a regular string...`);
+
+console.log(
+  "string with \n\
+multiple \n\
+lines"
+);
+
+console.log(`String
+multiple
+line`);
